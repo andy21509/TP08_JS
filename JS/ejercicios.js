66f1f54bfd0f6abf0793b710b5f21c0be478831e
ANDRES FURLAND OTERO <49437898@ort>
2025-09-10 14:20:42 -0300

    lucas

---
+++ JS/ejercicios.js
@@ -0,0 +1,8 @@
+function Ejercicio1(){
+    let nombre = document.getElementById("Ej1Nombre");
+    let fecha = document.getElementById("Ej1Fecha");
+    let resultado = document.getElementById("Ej1Resultado");
+
+    resultado.innerHTML = (fecha);
+    
+}
